76d8254196fb14855e95a82b94203d1cdbcd1d1c
hurbem <hurbem@gmail.com>
2026-06-02 12:04:32 -0300

    Add aggressive build script for pydantic-core

--- chatvgp/backend/build.sh
+++ chatvgp/backend/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-pip install --upgrade pip setuptools wheel
+set -e
+apt-get update
+apt-get install -y build-essential libpq-dev python3-dev
+pip install --upgrade pip setuptools wheel maturin
+
 pip install -r requirements.txt
