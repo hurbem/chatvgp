975c352852c7ea3fcefbb15cfc1b1726cc987422
hurbem <hurbem@gmail.com>
2026-06-02 12:11:12 -0300

    Use pre-compiled wheels for Render

--- chatvgp/backend/build.sh
+++ chatvgp/backend/build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -e
-apt-get update
-apt-get install -y build-essential libpq-dev python3-dev
-pip install --upgrade pip setuptools wheel maturin
-
-pip install -r requirements.txt
+pip install --upgrade pip wheel
+pip install -r requirements-render.txt
